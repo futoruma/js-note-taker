d0b316c44c6ca4fa76b0f1519c6ae9f8b44206dd
futoruma <evgeny.martov@gmail.com>
2023-01-11 22:57:41 +0900

    Made prettier.

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
+let addButton = document.querySelector("#addbutton");
+let nothingAdded = document.querySelector("#nothingadded");
 
-let addButton = document.querySelector("#addbutton")
-let nothingAdded = document.querySelector("#nothingadded")
-
-let noteCount = 0
+let noteCount = 0;
 
 addButton.addEventListener("click", () => {
-    nothingAdded.innerText = ""
-    noteCount++
+  nothingAdded.innerText = "";
+  noteCount++;
 
-    let textAreaInput = document.querySelector("#noteinput")
-    let newNoteFullText = textAreaInput.value
-    let newNoteShortText
-    if (newNoteFullText.length < 18) {
-        newNoteShortText = newNoteFullText
-    } else {
-        newNoteShortText = newNoteFullText.slice(0, 18).concat("...")
-    }
-    textAreaInput.value = null
+  let textAreaInput = document.querySelector("#noteinput");
+  let newNoteFullText = textAreaInput.value;
+  let newNoteShortText;
+  if (newNoteFullText.length < 18) {
+    newNoteShortText = newNoteFullText;
+  } else {
+    newNoteShortText = newNoteFullText.slice(0, 18).concat("...");
+  }
+  textAreaInput.value = null;
 
-    let notesDiv = document.querySelector("#notes")
-    let newNoteDiv = document.createElement("div")
-    notesDiv.append(newNoteDiv)
-    newNoteDiv.append(noteCount)
-    let breakTag = document.createElement("br")
-    newNoteDiv.append(breakTag)
-    newNoteDiv.append(newNoteShortText)
-    newNoteDiv.classList.add("newnote")
+  let notesDiv = document.querySelector("#notes");
+  let newNoteDiv = document.createElement("div");
+  notesDiv.append(newNoteDiv);
+  newNoteDiv.append(noteCount);
+  let breakTag = document.createElement("br");
+  newNoteDiv.append(breakTag);
+  newNoteDiv.append(newNoteShortText);
+  newNoteDiv.classList.add("newnote");
 
-    newNoteDiv.addEventListener("click", () => {
-        let popup = document.createElement("div")
-        document.body.append(popup)
-        popup.classList.add("popup")
-        popup.append(newNoteFullText)
+  newNoteDiv.addEventListener("click", () => {
+    let popup = document.createElement("div");
+    document.body.append(popup);
+    popup.classList.add("popup");
+    popup.append(newNoteFullText);
 
-        let overlay = document.createElement("div")
-        document.body.append(overlay)
-        overlay.classList.add("overlay")
+    let overlay = document.createElement("div");
+    document.body.append(overlay);
+    overlay.classList.add("overlay");
 
-        overlay.addEventListener("click", () => {
-            document.body.removeChild(popup)
-            document.body.removeChild(overlay)
-        })
-    })
-
-})
+    overlay.addEventListener("click", () => {
+      document.body.removeChild(popup);
+      document.body.removeChild(overlay);
+    });
+  });
+});
